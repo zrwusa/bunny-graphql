aa2d083f00474e95aa6f3513345313b41ef81aab
Revone <zrwusa@gmail.com>
2025-01-16 11:32:37 +1300

    feat: user validation

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as process from 'node:process';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -8,6 +9,7 @@ async function bootstrap() {
     origin: [process.env.FRONT_END],
     credentials: true,
   });
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT ?? 3000);
 }
 
--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,13 +1,17 @@
 import { Field, InputType } from '@nestjs/graphql';
+import { IsEmail, IsString, MinLength } from 'class-validator';
 
 @InputType()
 export class CreateUserDto {
   @Field()
+  @IsString()
+  @MinLength(3)
   username: string;
 
   @Field()
+  @IsEmail()
   email!: string;
 
-  @Field({ nullable: false })
+  @Field()
   password!: string;
 }
